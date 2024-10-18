dd2efd3fa8fa101b488f44ac99ee3bc9d4f4fb12
Letice Zwinger <leticedss@gmail.com>
2024-10-18 15:08:10 -0500

    create Schema

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 2, maxlength: 30 },
+  avatar: {
+    type: String,
+    required: [true, "the Avatar field is required"],
+    validate: {
+      validator(value) {
+        return;
+        validator.isURL(value);
+      },
+    },
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
