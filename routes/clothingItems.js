7e19729013571506ac06607ad1daeba79631ae47
Letice Zwinger <leticedss@gmail.com>
2025-01-14 18:07:17 -0300

    remove comments

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -17,26 +17,3 @@ router.put("/items/:itemId/likes", validateId, likeItem);
 router.delete("/items/:itemId/likes", validateId, unlikeItem);
 
 module.exports = router;
-
-// (err, req, res, next) => {
-//   if (isCelebrateError(err)) {
-//     // Handle Celebrate validation errors
-//     const validationErrors = {};
-//     for (const [segment, joiError] of err.details.entries()) {
-//       validationErrors[segment] = {
-//         message: joiError.message, // General validation message
-//         details: joiError.details.map((detail) => ({
-//           path: detail.path.join("."),
-//           message: detail.message,
-//         })),
-//       };
-//     }
-//     console.error("Validation Errors:", validationErrors);
-//     return res.status(400).json({
-//       status: "fail",
-//       message: "Validation failed",
-//       errors: validationErrors,
-//     });
-//   }
-//   next(err);
-// }
